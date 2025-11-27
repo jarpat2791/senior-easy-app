9b5aef276932549d58a2eefbea0932a6ee75bea9
jarpat2791 <jarpat2791@gmail.com>
2025-11-27 14:40:17 +0100

    Refactor service worker for improved caching
    Updated cache name and modified service worker event listeners for better logging and cache management.

--- service-worker.js
+++ service-worker.js
@@ -1,57 +1,56 @@
-const CACHE_NAME = 'senior-easy-v2';
-const BASE_PATH = '/senior-easy/';
-
+const CACHE_NAME = 'senior-easy-v2.1';
 const urlsToCache = [
-  BASE_PATH,
-  BASE_PATH + 'index.html',
-  BASE_PATH + 'manifest.json',
-  BASE_PATH + 'icon-192.png',
-  BASE_PATH + 'icon-512.png',
+  '/senior-easy-app/',
+  '/senior-easy-app/index.html',
+  '/senior-easy-app/manifest.json',
+  '/senior-easy-app/icons/icon-192.png',
+  '/senior-easy-app/icons/icon-512.png',
   'https://cdn.tailwindcss.com',
   'https://unpkg.com/react@18/umd/react.production.min.js',
   'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
   'https://unpkg.com/@babel/standalone/babel.min.js'
 ];
 
-self.addEventListener('install', event => {
+// Instalacja Service Worker
+self.addEventListener('install', function(event) {
+  console.log('Service Worker instalowany');
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting())
+      .then(function(cache) {
+        console.log('Otwarta cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener('activate', event => {
+// Aktywacja i czyszczenie starej cache
+self.addEventListener('activate', function(event) {
+  console.log('Service Worker aktywowany');
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then(function(cacheNames) {
       return Promise.all(
-        cacheNames.map(cacheName => {
+        cacheNames.map(function(cacheName) {
           if (cacheName !== CACHE_NAME) {
+            console.log('Usuwanie starej cache:', cacheName);
             return caches.delete(cacheName);
           }
         })
       );
-    }).then(() => self.clients.claim())
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
+// Interceptowanie requestów
+self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
+      .then(function(response) {
+        // Zwróć z cache lub wykonaj request sieciowy
         if (response) {
           return response;
         }
-        return fetch(event.request).then(response => {
-          if (!response || response.status !== 200 || response.type !== 'basic') {
-            return response;
-          }
-          const responseToCache = response.clone();
-          caches.open(CACHE_NAME).then(cache => {
-            cache.put(event.request, responseToCache);
-          });
-          return response;
-        });
-      })
+        return fetch(event.request);
+      }
+    )
   );
 });
